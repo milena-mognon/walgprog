7aea19b80c9d6a663824abdce8008c57bd175f14
Peterson <peterson.petio@gmail.com>
2019-07-17 21:45:18 -0300

    Add sections index content and links tests

--- spec/features/home/home_spec.rb
+++ spec/features/home/home_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe 'Home', type: :feature do
-  it 'have a title' do
-    visit root_path
+describe 'Sections:show', type: :feature do
+  let!(:event) { create(:event, :with_sections) }
 
-    text = 'Workshop de Ensino em Pensamento Computacional, Algoritmos e Programação'
-    expect(page).to have_text(text)
+  before(:each) do
+    visit root_path
+  end
+
+  context 'show section on index' do
+    it 'show' do
+      event.sections.each_with_index do |section, i|
+        expect(page).to have_content(section.title)
+        #expect(page).to have_content(section.content)
+        #expect(icon_class).to eq(section.icon)
+      end
+    end
+  end
+
+  context 'icons with links' do
+    it do
+      event.sections.each_with_index do |section, i|
+        expect(page).to have_link((section.id))
+      end
+    end
   end
 end
